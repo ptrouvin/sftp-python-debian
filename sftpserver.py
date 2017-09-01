114d9fb547530ac1cfa76a2980d1bf0c05da1589
pascal.trouvin <pascal.trouvin.exmc@axa-tech.com>
2017-09-01 09:58:50 +0200

    Fix bug with ROOT

--- sftpserver.py
+++ sftpserver.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+# License: Apache
+
+# Author: Pascal TROUVIN
+
+# History:
+# 2017-08-31 pt: add IP whiltelist
+# 2017-08-31 pt: add multithreading
+# 2017-08-31 pt: Add password as authentication method
+
+
 import os
 import time
 import socket
@@ -8,33 +18,55 @@ import sys
 import textwrap
 import paramiko
 import logging
+import threading
 
 HOST, PORT = '0.0.0.0', 3373
 RSA_LENGTH = 4096
 BACKLOG = 10
 log = logging.getLogger('sftpserver')
-# logging.basicConfig(level=logging.INFO,
-                  # format='%(asctime)s %(levelname)s: %(message)s',
-                  # datefmt='%Y-%m-%d %H:%M:%S')
+#logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 LOGGING_LEVELS = {'critical': logging.CRITICAL,
                   'error': logging.ERROR,
                   'warning': logging.WARNING,
                   'info': logging.INFO,
                   'debug': logging.DEBUG}
 
+# https://stackoverflow.com/questions/9590965/convert-an-ip-string-to-a-number-and-vice-versa#29482851
+ip2int = lambda ip: reduce(lambda a, b: (a << 8) + b, map(int, ip.split('.')), 0)
+int2ip = lambda n: '.'.join([str(n >> (i << 3) & 0xFF) for i in range(0, 4)[::-1]])                  
+
 class StubServer (paramiko.ServerInterface):
     def __init__(self, users):
+        global log
         self.users=users
+        self.hasPassword=False
+        self.hasPubkey=False
+        methods=""
+        for u in self.users.keys():
+            if self.users[u].get('RSA:',False):
+                self.hasPubkey=True
+                methods=methods+"publickey "
+            if self.users[u].get('PWD:',False):
+                self.hasPassword=True
+                methods=methods+"password "
+            if self.hasPassword and self.hasPubkey:
+                break
+        if not self.hasPassword and not self.hasPubkey:
+            raise Exception('No Authentication method')
+        log.info("Authentication method enabled: "+methods)
         super(paramiko.ServerInterface, self).__init__()
         
     def check_auth_password(self, username, password):
-        # all are disallowed
+        log.debug("check_auth_password(%s,%s)" % (username,password))
+        if self.users.has_key(username) and self.users[username].get('PWD:',False)==password:
+            log.info("User %s authenticated by password" % username )
+            return paramiko.AUTH_SUCCESSFUL
         return paramiko.AUTH_FAILED
         
     def check_auth_publickey(self, username, key):
         keyb64=key.get_base64()
         log.debug("check_auth_publickey(%s,%s)" % (username,keyb64))
-        if self.users.has_key(username) and self.users[username]==keyb64:
+        if self.users.has_key(username) and self.users[username].get('RSA:',False)==keyb64:
             log.info("User %s authenticated by publicKey" % username )
             return paramiko.AUTH_SUCCESSFUL
         return paramiko.AUTH_FAILED
@@ -44,7 +76,15 @@ class StubServer (paramiko.ServerInterface):
 
     def get_allowed_auths(self, username):
         """List availble auth mechanisms."""
-        return "publickey"
+        method=""
+        sep=""
+        if self.users.get(username,False) and self.users[username].get('RSA:',False):
+            method=method+sep+"publickey"
+            sep=','
+        if self.users.get(username,False) and self.users[username].get('PWD:',False):
+            method=method+sep+"password"
+            sep=','
+        return method
         
 
 class StubSFTPHandle (paramiko.SFTPHandle):
@@ -68,7 +108,7 @@ class StubSFTPServer (paramiko.SFTPServerInterface):
     # assume current folder is a fine root
     # (the tests always create and eventualy delete a subfolder, so there shouldn't be any mess)
     
-    ROOT = os.getcwd() # will be overriden by argparse
+    #ROOT = os.getcwd() # will be overriden by argparse
     
     def _realpath(self, path):
         return self.ROOT + self.canonicalize(path)
@@ -219,7 +259,20 @@ class StubSFTPServer (paramiko.SFTPServerInterface):
                 symlink = '<error>'
         return symlink
 
-def start_server(host, port, keyfile, level, users):
+def start_connection(conn, host_key, users):
+    transport = paramiko.Transport(conn)
+    transport.add_server_key(host_key)
+    transport.set_subsystem_handler(
+        'sftp', paramiko.SFTPServer, StubSFTPServer)
+
+    server = StubServer(users)
+    transport.start_server(server=server)
+
+    channel = transport.accept()
+    while transport.is_active():
+        time.sleep(1)
+        
+def start_server(host, port, keyfile, level, users, ip_whilelist=[]):
     paramiko.common.logging.basicConfig(level=level)
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -227,29 +280,38 @@ def start_server(host, port, keyfile, level, users):
     server_socket.bind((host, port))
     server_socket.listen(BACKLOG)
 
+    if keyfile!=None:
+        host_key = paramiko.RSAKey.from_private_key_file(keyfile)
+    else:
+        log.info("Generate a new SSH hostkey length=%d" % RSA_LENGTH)
+        host_key = paramiko.RSAKey.generate(RSA_LENGTH)
+
     while True:
         conn, addr = server_socket.accept()
-
-        if keyfile!=None:
-            host_key = paramiko.RSAKey.from_private_key_file(keyfile)
-        else:
-            log.info("Generate a new SSH hostkey length=%d" % RSA_LENGTH)
-            host_key = paramiko.RSAKey.generate(RSA_LENGTH)
+        
+        (ip,port)=addr
+        
+        log.info("Connection from: %s:%d" % addr)
+        
+        if len(ip_whilelist):
+            ip_authorized=False
+            for i,n in ip_whilelist:
+                if (ip2int(ip) & n) == i:
+                    ip_authorized=True
+                    log.info("%s is authorized by IP_FILTERING %08X/%08X" % (ip,i,n))
+                    break
+            if not ip_authorized:
+                log.info("%s is not authorized by IP_WHITELIST" % ip)
+                conn.close()
+                continue
         
         try:
-            transport = paramiko.Transport(conn)
-            transport.add_server_key(host_key)
-            transport.set_subsystem_handler(
-                'sftp', paramiko.SFTPServer, StubSFTPServer)
-
-            server = StubServer(users)
-            transport.start_server(server=server)
-
-            channel = transport.accept()
-            while transport.is_active():
-                time.sleep(1)
+            pid=threading.Thread(target=start_connection, args=(conn, host_key, users) )
+            pid.start()
         except Exception as e:
             log.error(str(e))
+            
+        log.info("Current threads: %d" % len(threading.enumerate()))
                 
 if __name__ == '__main__':
     
@@ -280,12 +342,12 @@ if __name__ == '__main__':
 
     class customAction(argparse._AppendAction):
         def __call__(self,parser,args,values,option_string=None):
-            kv=re.compile("[=:]").split(values)
-            if len(kv)!=2:
+            kv=re.compile("^([^=:]+)[=:](RSA:|PWD:|)(.*)$").match(values)
+            if not kv:
                 log.critical("INVALID-USER: "+values)
                 parser.print_help()
                 sys.exit(-1)
-            super(customAction, self).__call__(parser,args,{kv[0]:kv[1]},option_string)
+            super(customAction, self).__call__(parser,args,{kv.group(1):kv.group(2)+kv.group(3)},option_string)
 
 	
     parser = argparse.ArgumentParser(usage=textwrap.dedent(usage))
@@ -297,6 +359,10 @@ if __name__ == '__main__':
         '-p', '--port', dest='port', type=int, default=PORT,
         help='listen on PORT [default: %(default)d]'
     )
+    parser.add_argument(
+        '--ip', action='append', default=[],
+        help='IPv4 whiltelist, format: IP/length or IP'
+    )
     parser.add_argument('-l','--level', default='info',
             help='Logging level '+','.join(LOGGING_LEVELS.keys()))
     parser.add_argument(
@@ -305,7 +371,7 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '-U', '--user', action=customAction,
-        help='store users and publicKey:  user:pubkey, can be completed by environment variable USERS'
+        help='store users and publicKey:  user:pubkey or user:RSA:pubkey or user:PWD:password, can be completed by environment variable USERS'
     )
     parser.add_argument(
         '--root', action='store',
@@ -323,11 +389,16 @@ if __name__ == '__main__':
 
     users={}
     if args.user:
+        patMethod=re.compile("^(RSA:|PWD:|)(.*)$")
         for i in xrange(len(args.user)):
             for u in args.user[i].keys():
-                users[u]=args.user[i][u]
+                m=patMethod.match(args.user[i][u])
+                if m:
+                    users[u]={m.group(1): m.group(2)}
+                else:
+                    users[u]={'RSA': args.user[i][u]}
     # check if USERS from environment
-    patUser=re.compile("([^:=]+)[:=](.*)")
+    patUser=re.compile("^([^:=]+)[:=](RSA:|PWD:|)(.*)$")
     for user in re.compile("[,; ]").split(os.environ.get('USERS','')):
         if user:
             m = patUser.match(user)
@@ -335,27 +406,70 @@ if __name__ == '__main__':
                 log.error("USER '"+user+"' is invalid")
                 print_help()
                 sys.exit(-1)
-            (u,k)=m.groups()
-            users[u]=k
+            (u,method,k)=m.groups()
+            if len(method)==0:
+                method='RSA:'
+            users[u]={method: k}
             
     if users is None:
         parser.print_help()
         sys.exit(-1)
     
-    if os.environ.get('ROOT',False):
-        StubSFTPServer.ROOT=os.environ['ROOT']
-    elif args.root is not None:
+    if args.root is not None:
         StubSFTPServer.ROOT=args.root
+    elif os.environ.get('ROOT',False):
+        StubSFTPServer.ROOT=os.environ['ROOT']
+    else: 
+        StubSFTPServer.ROOT = os.getcwd()
     
     port=args.port
     if os.environ.get('PORT',False):
         port=int(os.environ['PORT'])
+
+    ip_whilelist=[]
+    if args.ip:
+        patWH=re.compile('^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})/([0-9]{1,2})$')
+        patIP=re.compile('^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$')
+        for wh in args.ip:
+            m=patWH.match(wh)
+            if not m:
+                m=patIP.match(wh)
+                le=32
+                if not m:
+                    raise Exception("--ip=%s SYNTAX ERROR" % wh)
+            else:
+                le=int(m.group(5))
+            ad=(int(m.group(1)) & 255)<<24 | (int(m.group(2)) & 255)<<16 | (int(m.group(3)) & 255)<<8 | (int(m.group(4)) & 255)
+            if le<0 or le>32:
+                raise Exception("--ip=%s INVALID LENGTH" % wh)
+            if le==0:
+                raise Exception("--ip=%s zero length means IP_FILTERING disabled, so why use it?" % wh)
+            # hostmask
+            mh=2**(32-le)-1
+            # networkmask
+            mn=0xFFFFFFFF ^ mh
+            # clearing host part
+            if le<32:
+                ad= ad & mn
+            # verify that the address/len is not already included
+            found=False
+            for i in xrange(len(ip_whilelist)):
+                (a,n)=ip_whilelist[i]
+                if (a & mn) == ad:
+                    log.debug("ip filter: %08X/%08X replaced by %08X/%08X" % (a,n,ad,mn))
+                    ip_whilelist[i]=(ad,mn)
+                    found=True
+            if not found:
+                ip_whilelist.append((ad,mn))
+                log.debug("ip filter: %08X/%08X" % (ad,mn))
+                
+        
         
     log.info("SFTP server root directory set to: %s" % StubSFTPServer.ROOT)
     log.info("SFTP server port: %s:%d" % (args.host,port))
         
     log.debug("USERS: " + json.dumps(users, indent=4, sort_keys=True))
 
-    start_server(args.host, port, args.keyfile, level, users)
+    start_server(args.host, port, args.keyfile, level, users, ip_whilelist)
 
 
